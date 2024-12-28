84c2fb1f3e35e3a38c499999ab8d5b660f157b23
Rasadul Islam <rasadulislam.cse@gmail.com>
2024-12-29 01:51:53 +0600

    my marathon list get added

--- index.js
+++ index.js
@@ -16,55 +16,68 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const marathonCollection = client.db('marathonDB').collection('marathon');
+        const marathonCollection = client.db('marathonDB').collection('marathon');
 
-    app.post('/marathons', async(req, res)=>{
-        const newMarathon = req.body;
-        console.log(newMarathon);
-        const result = await marathonCollection.insertOne(newMarathon);
-        res.send(result);
-    })
+        // Add marathon data
+        app.post('/marathons', async (req, res) => {
+            const newMarathon = req.body;
+            console.log(newMarathon);
+            const result = await marathonCollection.insertOne(newMarathon);
+            res.send(result);
+        })
 
-    app.get('/marathons', async (req, res) => {
-        const cursor = marathonCollection.find().limit(6);
-        const result = await cursor.toArray();
-        res.send(result);
-      })
-      app.get('/marathons/:id', async(req, res)=>{
-        const id = req.params.id;
-        const query ={_id: new ObjectId(id)}
-        const result =await marathonCollection.findOne(query);
-         res.send(result);
-      })
+        // All marathon
+        app.get('/marathons', async (req, res) => {
+            const cursor = marathonCollection.find().limit(6);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // see more ditails
+        app.get('/marathon/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await marathonCollection.findOne(query);
+            res.send(result);
+        })
+
+        // my marathon list
+        app.get('/marathons/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { userEmail: email }
+            const result = await marathonCollection.find(query).toArray();
+            res.send(result);
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
-    res.send ('Marathon server is Running')
+app.get('/', (req, res) => {
+    res.send('Marathon server is Running')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Marathon server is running on port : ${port}`)
 })
