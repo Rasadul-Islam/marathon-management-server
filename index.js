39392d04993a83ef1516cd63481f3a5b2c9f8bf2
Rasadul Islam <rasadulislam.cse@gmail.com>
2024-12-31 14:07:06 +0600

    register delete form database

--- index.js
+++ index.js
@@ -95,6 +95,14 @@ async function run() {
             res.send(result);
         })
 
+        // my regitration delete
+        app.delete('/marathons-register/:id',async(req, res)=>{
+            const id =req.params.id;
+            const query={_id: new ObjectId(id)}
+            const result= await marathonRegisterCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
